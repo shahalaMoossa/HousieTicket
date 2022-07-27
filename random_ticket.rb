63dc50e49d93541c0e803a9712c1addbf609cf7f
Shahala Moossa K K <42081892+shahalaMoossa@users.noreply.github.com>
2022-07-27 14:28:22 +0530

    add comments to the code

--- random_ticket.rb
+++ random_ticket.rb
@@ -5,12 +5,17 @@ def gen_tckt
 	min_limit = 0
 	max_limit = 9 
 	9.times{
-		min_limit = 1 if min_limit == 0
+		min_limit = 1 if min_limit == 0	
 		max_limit = 90 if max_limit == 89
+		
+		#Generate three random nubmers between the limits and sort it and push each one of them to three different arrays,
+		#thus, at last we get three arrays with 9 elements each.
 		temp = (min_limit..max_limit).to_a.sample(3).sort
 		arr1.push(temp[0])
 		arr2.push(temp[1])
 		arr3.push(temp[2])
+		
+		#increment the limits by 10
 		min_limit += 10
 		max_limit += 10
 	}
@@ -20,10 +25,12 @@ def gen_tckt
 end
 
 def put_blank_and_print(arr)
+	#fetch 4 random numbers from the given array and replace those with "X"
 	temp = arr.sample(4)
 	temp.each do |val|
 		arr[arr.index(val)] = "X"
 	end
+	#print each of the elements of the array to the console
 	arr.each {|a| print "#{a}\t"}
 	print "\n"
 end
